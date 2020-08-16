39aca929a075f537de18591398fb4557a190ce40
MarLMazo <marlmazo01@gmail.com>
2020-08-15 22:43:14 -0400

    update temporary routing security

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/Login.vue";
+import Login from "../components/Login.vue";
 import Register from "../components/Register.vue";
 import Chat from "../views/Chat.vue";
 //import Chat from "../components/CreateMessage.vue";
@@ -7,6 +7,10 @@ import Chat from "../views/Chat.vue";
 const routes = [
   {
     path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "/login",
     name: "Login",
     component: Login
   },
@@ -22,28 +26,14 @@ const routes = [
     props: true,
     //need to protect
     beforeEnter: (to, from, next) => {
-      if (to.params.name) {
+      //console.log(to);
+      if (from.fullPath == '/login') {
         next();
       } else {
         next({name: 'Login'})
       }
     }
   }
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Home
-  // },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
-
 ];
 
 const router = createRouter({
