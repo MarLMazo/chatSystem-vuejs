91d303a710c0579bca4a07b2cb27515b99c95e49
MarLMazo <marlmazo01@gmail.com>
2020-08-19 21:31:45 -0400

    added logout component

---
+++ src/components/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+    methods:{
+        signOut(){
+            firebase
+                .auth()
+                .signOut()
+                .then(()=>{
+                    this.$router.replace({
+                        name: "Login"
+                    })
+                })
+        }
+    }
+}
+</script>
